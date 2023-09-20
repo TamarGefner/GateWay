e0da85c940c545e9a382acbe5d0673f38dd74418
Saritik <tik@g.jct.ac.il>
2023-09-21 02:06:10 +0300

    finish imagga service

---
+++ GateWay/Controllers/ImaggaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using GateWay.Models;
+
+namespace GateWay.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImaggaController : ControllerBase
+    {
+        [HttpGet("CheckImage")]
+        public async Task<IActionResult> CheckImage([FromQuery] string imageUrl)
+        {
+            try
+            {
+                ImaggaModel imaggaModel = new ImaggaModel();
+
+                bool tagExists = await imaggaModel.CheckImage(imageUrl);
+
+                // Return appropriate messages based on the result
+                if (tagExists)
+                {
+                    return Ok("Okay, the image contains coffee");
+                }
+                else
+                {
+                    return Ok("Problem, this is not an image of coffee");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that might occur during the request
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ GateWay/Models/ImaggaModel.cs
@@ -0,0 +1,67 @@
+﻿// This examples is using RestSharp as a REST client - http://restsharp.org
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Text;
+
+
+namespace GateWay.Models
+{
+    public class ImaggaModel
+    {
+        public async Task<bool> CheckImage(string imageUrl)
+        {
+            string apiKey = "acc_99243853ec106fb";
+            string apiSecret = "e158559864c958a22ea9325862b3eeb4";
+
+            if (string.IsNullOrEmpty(imageUrl))
+            {
+                throw new Exception("Image URL is required.");
+            }
+
+            string basicAuthValue = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{apiKey}:{apiSecret}"));
+
+            var client = new RestClient("https://api.imagga.com/v2/tags");
+            var request = new RestRequest();
+            request.AddParameter("image_url", imageUrl);
+            request.AddHeader("Authorization", $"Basic {basicAuthValue}");
+
+            RestResponse response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                // Parse the JSON response
+                var jsonResponse = JObject.Parse(response.Content);
+
+                // Extract the tags in English ("en")
+                var tags = jsonResponse["result"]["tags"];
+
+                // Create a list to store the tags
+                var tagList = new List<string>();
+
+                foreach (var tag in tags)
+                {
+                    var tagName = tag["tag"]["en"].ToString();
+                    tagList.Add(tagName);
+                }
+
+                // Define the tags to check for
+                var tagsToCheck = new List<string> { "coffee" };
+
+                // Check if any of the desired tags exist in the list
+                foreach (var tagToCheck in tagsToCheck)
+                {
+                    if (tagList.Contains(tagToCheck))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else
+            {
+                // Handle API request failure
+                throw new Exception("API request failed.");
+            }
+        }
+    }
+}

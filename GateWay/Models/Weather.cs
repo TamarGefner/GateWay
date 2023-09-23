0dc41a3f9e42612d0d8ec4d41d556b0f75253f38
AYALA HAIM <ay4117096@gmail.com>
2023-09-23 21:11:14 +0300

    add a openweatheramp service

---
+++ GateWay/Models/Weather.cs
@@ -0,0 +1,29 @@
+﻿namespace GateWay.Models
+{
+
+    public class WeatherMain
+    {
+        public double Temp { get; set; }
+        public double FeelsLike { get; set; }
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+    }
+
+    public class WeatherDescription
+    {
+        public string Main { get; set; } // Main weather description (e.g., "Clear")
+        public string Description { get; set; } // Detailed weather description (e.g., "clear sky")
+    }
+
+    public class WeatherResponse
+    {
+        public WeatherMain Main { get; set; }
+        public List<WeatherDescription> Weather { get; set; }
+    }
+
+
+}
+
+
---
+++ GateWay/Models/WeatherModel.cs
@@ -0,0 +1,47 @@
+﻿using GateWay.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+
+namespace GateWay.Models
+{
+    public class WeatherModel
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey = "fef44efb89776abd3da0b74184d8a743"; // Replace with your OpenWeatherMap API key
+        private readonly string _weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+        public WeatherModel()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<WeatherResponse> GetWeatherForCity(string cityName)
+        {
+            try
+            {
+                string apiUrl = $"{_weatherApiUrl}?q={cityName}&appid={_apiKey}";
+
+                var response = await _httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(content);
+                    return weatherResponse;
+                }
+
+                return null;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
+
+
+
+
